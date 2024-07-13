caacfd5d5810d7c62dd96fb174eb5580f9a26a24
Ch40s1 <114247747+Ch40s1@users.noreply.github.com>
2024-07-12 23:07:57 -0500

    added way to update tasks

--- cli-todo/main.go
+++ cli-todo/main.go
@@ -44,8 +44,8 @@ func viewTasks(list []string) {
 	if len(list) == 0 {
 		fmt.Println("No items in list")
 	} else {
-		for _, item := range list {
-			fmt.Printf("%s\n", item)
+		for i, item := range list {
+			fmt.Printf("%d: %s\n", i+1, item)
 		}
 	}
 }
@@ -57,7 +57,18 @@ func createTask(list []string) []string {
 	return append(list, results)
 }
 func updateTask(list []string) {
-	// pass
+	viewTasks(list)
+	fmt.Println("Which task would you like to modify?enter number of task.")
+	var userData int
+	fmt.Scanln(&userData)
+	if userData < 0 || userData < len(list) {
+		fmt.Println("please enter number of a task.")
+	} else {
+		fmt.Println("Enter update:")
+		modify, _ := bufio.NewReader(os.Stdin).ReadString('\n')
+		results := strings.TrimSuffix(modify, "\n")
+		list[userData-1] = results
+	}
 }
 func deleteTask(list []string) {
 	//pass
